5970a879a9ef2565d5e74d63f2d36b235b176040
Adam <a.niewczas01@gmail.com>
2024-11-24 15:30:58 +0100

    feat: Add searching methods for organisation

--- src/main/java/org/example/springboottesting/repository/ProjectRepository.java
+++ src/main/java/org/example/springboottesting/repository/ProjectRepository.java
@@ -3,5 +3,10 @@ package org.example.springboottesting.repository;
 import org.example.springboottesting.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findByOrganisationId(int organisationId);
+
 }
