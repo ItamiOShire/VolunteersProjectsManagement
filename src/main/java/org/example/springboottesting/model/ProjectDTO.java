ebce6f7872606b32e72d1891e70cf4dfadf5df41
Adam <a.niewczas01@gmail.com>
2024-11-26 16:58:01 +0100

    feat: Create DTO for displaying only necessary project information

---
+++ src/main/java/org/example/springboottesting/model/ProjectDTO.java
@@ -0,0 +1,56 @@
+package org.example.springboottesting.model;
+
+import java.util.List;
+
+public class ProjectDTO {
+
+    private String desc;
+
+    private String imgPath;
+
+    private String title;
+
+    private String orgName;
+
+    private List<Tag> tags;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+}
--- src/main/java/org/example/springboottesting/repository/OrganisationRepository.java
+++ src/main/java/org/example/springboottesting/repository/OrganisationRepository.java
@@ -2,7 +2,12 @@ package org.example.springboottesting.repository;
 
 import org.example.springboottesting.model.Organisation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface OrganisationRepository extends JpaRepository<Organisation, Long> {
+
+    @Query("SELECT o.orgName FROM Organisation o WHERE o.id = :id")
+    public String getNameById(@Param("id") int id);
 }
