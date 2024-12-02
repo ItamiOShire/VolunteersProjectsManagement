0eccd0f9753ca631bdd4ff9132e25e3eb51180aa
Adam <a.niewczas01@gmail.com>
2024-12-02 16:58:58 +0100

    feat: Add VolunteerDTO

---
+++ src/main/java/org/example/springboottesting/DTO/VolunteerDTO.java
@@ -0,0 +1,54 @@
+package org.example.springboottesting.DTO;
+
+public class VolunteerDTO {
+
+    private String fnameAndLname;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
+
+    private int age;
+
+    public String getFnameAndLname() {
+        return fnameAndLname;
+    }
+
+    public void setFnameAndLname(String fnameAndLname) {
+        this.fnameAndLname = fnameAndLname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
