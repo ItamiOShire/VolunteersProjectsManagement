a5f743782c7d26221efdeaae51797a54acd34a09
Adam <a.niewczas01@gmail.com>
2024-12-12 23:17:57 +0100

    fix and ref: Added cascading delete in database

--- src/main/java/org/example/springboottesting/repository/TaskRepository.java
+++ src/main/java/org/example/springboottesting/repository/TaskRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    // TODO: add manually implementation of deleting only link between Volunteer_Task
     List<Task> getTasksByProjectId(int projectId);
 }
